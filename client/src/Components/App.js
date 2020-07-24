7a9ac49d33844d73757587e99dd7a6c5e8f88809
Maximilian Skoglund Lartell <max.lartell@gmail.com>
2020-07-24 09:11:38 +0200

    friday2

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Matches from './Matches';
+import { Col, Row, Container } from 'reactstrap';
+import Profileother from './Profileother';
+import '../App.css';
 
 function App() {
+
+  const [state, setState] = useState({ user: '1', clickedMatch: null });
+  
+  const getMatchData = async (event) => await setState({ clickedMatch: event.target.id });
+  
+  const getFirstMatch = async (id) =>  await setState({ clickedMatch: id });
+
   return (
     <div className="App">
-    <Matches userId="1"/>
+    <Container className="themed-container">.
+      <Row>
+        <Col>.col</Col>
+
+        <Col>
+        <Profileother matchId={state.clickedMatch}/>
+        </Col>
+
+        <Col><Matches getFirstMatch={getFirstMatch} getMatchData={getMatchData} userId={state.user}/></Col>
+      </Row>
+    </Container>
     </div>
   );
 }
