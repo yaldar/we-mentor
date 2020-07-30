3a250624e3349d65cfd95127b193a3d444f3c5ed
FannyPetersen <64142959+FannyPetersen@users.noreply.github.com>
2020-07-30 10:51:51 +0200

    building

---
+++ client/build/precache-manifest.2a85c2d70a0f4d98e808baea3ffa1805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58aa824ce0c9a77f646c9de25cb4f06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c26965558697c94d410",
+    "url": "/static/css/main.30281b9e.chunk.css"
+  },
+  {
+    "revision": "921445922bdd5c64a3b2",
+    "url": "/static/js/2.93d6f008.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.93d6f008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c26965558697c94d410",
+    "url": "/static/js/main.5ff7b1c4.chunk.js"
+  },
+  {
+    "revision": "fbd28fdf78aeca92aea3",
+    "url": "/static/js/runtime-main.6ff7427c.js"
+  }
+]);
